03a7803647aa67dc88bb14c698f6883d3c83341f
murraygi <133593977+murraygi@users.noreply.github.com>
2024-05-16 12:45:23 +0100

    Add files via upload
    12 Apr 24

---
+++ calculator.js
@@ -0,0 +1,63 @@
+//BASIC CALCULATOR
+
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
+// function subtract(num1, num2) {
+//     return num1 - num2;
+// }
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
+// function divide(num1, num2) {
+//     return num1 / num2;
+// }
+// function modulo(num1, num2) {
+//     return num1 % num2;
+// }
+// function percentage(num1, num2) {
+//     return (num1 * num2)/100;
+// }
+// function calculator(num1, num2, operator) {
+//     return operator(num1, num2);
+// }
+
+//OBJECT CALCULATOR
+
+// function calculator(num1, num2, operation) {
+//     const operations = {
+//         add: (num1, num2) => num1 + num2,
+//         subtract: (num1, num2) => num1 - num2,
+//         multiply: (num1, num2) => num1 * num2,
+//         divide: (num1, num2) => num1 / num2,
+//         modulo: (num1, num2) => num1 % num2,
+//         percentage: (num1, num2) => (num1 * num2) / 100,
+//     };
+
+//     if (!operations[operation]) {
+//         throw new Error("Invalid operation");
+//     }
+
+//     return operations[operation](num1, num2);
+// }
+
+//SWITCH STATEMENT CALCULATOR
+
+// function calculator(num1, num2, operator) {
+//     switch(operator) {
+//         case 'add':
+//             return num1 + num2;
+//         case 'subtract':
+//             return num1 - num2;
+//         case 'multiply':
+//             return num1 * num2;
+//         case 'divide':
+//             return num1 / num2;
+//         case 'modulo':
+//             return num1 % num2;
+//         case 'percentage':
+//             return (num1 * num2) / 100;
+//         default:
+//             throw new Error('Invalid operator');
+//     }
+// }
